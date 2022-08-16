382597b031dff5ae2eaec10990a851fc38138d0d
Oliver Dippel <o.dippel@gmx.de>
2022-08-16 22:22:32 +0200

    update setup-py

--- setup.py
+++ setup.py
@@ -6,17 +6,17 @@ import os
 from setuptools import setup
 
 setup(
-    name='dxfbox',
+    name='ttf2dxf',
     version='0.1.0',
     author='Oliver Dippel',
     author_email='o.dippel@gmx.de',
-    packages=['dxfbox'],
-    scripts=['bin/dxfbox'],
-    url='https://github.com/multigcs/dxfbox/',
+    packages=['ttf2dxf'],
+    scripts=['bin/ttf2dxf'],
+    url='https://github.com/multigcs/ttf2dxf/',
     license='LICENSE',
-    description='simple python based dxf box generator',
+    description='simple python based ttf to dxf converter',
     long_description=open('README.md').read(),
-    install_requires=['ezdxf'],
+    install_requires=['ezdxf', 'freetype-py'],
     include_package_data=True,
     data_files = []
 )
